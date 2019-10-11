eb279bcc8512ff72739f283651cfd103b8675c19
Clément Dessoude <clement.dessoude@gmail.com>
2019-10-11 11:26:35 +0200

    update with latest changes from cypress module

--- generators/entity-client/index.js
+++ generators/entity-client/index.js
@@ -1,6 +1,13 @@
 /* eslint-disable consistent-return */
+const chalk = require('chalk');
+
 const EntityClientGenerator = require('generator-jhipster/generators/entity-client');
+const constants = require('generator-jhipster/generators/generator-constants');
+const utils = require('generator-jhipster/generators/utils');
+
 const writeFiles = require('./files').writeFiles;
+
+const TEST_SRC_DIR = constants.CLIENT_TEST_SRC_DIR;
 
 module.exports = class extends EntityClientGenerator {
     constructor(args, opts) {
@@ -20,5 +27,24 @@ module.exports = class extends EntityClientGenerator {
     get end() {
         // Here we are not overriding this phase and hence its being handled by JHipster
         return super._end();
+    }
+
+    addEntitySupportFileToIndex() {
+        if (this.e2eTestsFramework.includes('cypress')) {
+            try {
+                const supportFileIndex = `${TEST_SRC_DIR}cypress/support/index.ts`;
+                const importEntry = 'import \'./entities\';';
+                utils.rewriteFile({
+                    file: supportFileIndex,
+                    needle: 'jhipster-needle-add-support-file',
+                    splicable: [
+                        this.stripMargin(importEntry)
+                    ]
+                }, this);
+            } catch (e) {
+                this.log(`${chalk.yellow('Cypress: Entities support file not added to index.js.\n')}`);
+                this.debug('Error:', e);
+            }
+        }
     }
 };
